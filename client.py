e12cb2a06118fc8ac97beb4e29912f2bfa842b6d
Gregorio Robles <grex@computer.org>
2015-11-19 11:47:31 +0100

    Moved to Python3

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 Programa cliente que abre un socket a un servidor
 """
@@ -20,13 +20,13 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+print("Enviando: " + LINE)
+my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+print('Recibido -- ', data.decode('utf-8'))
+print("Terminando socket...")
 
 # Cerramos todo
 my_socket.close()
-print "Fin."
+print("Fin.")
--- server.py
+++ server.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
-import SocketServer
+import socketserver
 
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
+            print("El cliente nos manda " + line.decode('utf-8'))
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
@@ -26,6 +26,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
