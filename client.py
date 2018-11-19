57397567cbe4374158c65ce3c92793f61c18d064
Yolanda Lillo Mata <ylillo@gamma19.aulas.gsyc.urjc.es>
2018-11-19 09:57:02 +0100

    RECEPTOR Y PUERTO

--- client.py
+++ client.py
@@ -5,6 +5,15 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
+
+try:
+    METODO = sys.argv[1] #Método SIP
+    RECEPTOR = sys.argv[2].split(':')[0]  #Receptor@IP
+    PUERTO = sys.argv[2].split(':')[1]
+
+except ValueError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPpor")
 
 # Cliente UDP simple.
 
@@ -25,6 +34,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
+    print("EL RECEPTOR ES",RECEPTOR)
+    print("EL PUERTO ES", PUERTO)
     print("Terminando socket...")
 
 print("Fin.")
