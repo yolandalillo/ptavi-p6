df1236db1d9a58335b50960a4a5a394a51234e6e
Yolanda Lillo Mata <ylillo@alpha30.aulas.gsyc.urjc.es>
2018-11-19 15:11:16 +0100

    Peticiones SIP

--- client.py
+++ client.py
@@ -10,36 +10,29 @@ import sys
 try:
     METHOD = sys.argv[1] #Método SIP
     RECEPTOR = sys.argv[2].split(':')[0]  # Receptor@IP.
-    IP = RECEPTOR.split('@')[-1] # IP.
+    IP = RECEPTOR.split('@')[-1] # Dirección IP.
     PORT = int(sys.argv[2].split(':')[1]) # PuertoSIP.
     
 except (IndexError, ValueError):
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IP, PORT))
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-
-
-
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP, PORT))
-
-
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    try:
-        print("Enviando: " + LINE)
-        data = my_socket.recv(1024)
-        print('Recibido -- ', data.decode('utf-8'))
-        print("EL RECEPTOR ES",RECEPTOR)
-        print("EL PUERTO ES", PORT)
+        if METHOD == 'INVITE' or METHOD == 'BYE':
+            line = (METHOD + ' sip:' + RECEPTOR + ' SIP/2.0\r\n\r\n')
+            print(line)
+            my_socket.send(bytes(line, 'utf-8'))
+            data = my_socket.recv(1024)
+            print(data.decode('utf-8'))
+            codigo = data.decode('utf-8').split()[-2]
+            if codigo == '200':
+                line = ('ACK' + ' sip:' + RECEPTOR + ' SIP/2.0\r\n\r\n')
+                my_socket.send(bytes(line, 'utf-8'))
+                data = my_socket.recv(1024)
         print("Terminando socket...")
-        print("Fin.")
-    except ConnectionRefusedError:
-        print("ERROR DE CONEXIÓN")
+
+except ConnectionRefusedError:
+    print("Error de conexión")
