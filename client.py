0de65f37c8dacf5be60df5217a39cb69a15554e8
Yolanda Lillo Mata <ylillo@gamma18.aulas.gsyc.urjc.es>
2018-11-26 10:29:56 +0100

    peps pasados

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor."""
 
 import socket
 import sys
